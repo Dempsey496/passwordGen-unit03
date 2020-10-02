fac58c4bd45f83b0e2955ac793a8132a05ca3a0c
Dempsey FP <dempseyfinley@gmail.com>
2020-10-01 23:50:43 -0400

    First prompt and arrays set

--- script.js
+++ script.js
@@ -15,42 +15,33 @@
 // WHEN the password is generated
 // THEN the password is either displayed in an alert or written to the page
 // ```
-
-var size = document.getElementById("length");
-var upperCheck = document.getElementById("upper");
-var lowerCheck = document.getElementById("lower");
-var numCheck = document.getElementById("number");
-var symbolCheck = document.getElementById("symbol");
 var genBtnEl = document.getElementById("generate");
 var charSet = "";
+var passLength = "";
 
 var upperArray = "A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z,";
 var lowerArray = "a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z,";
 var symbolArray = "!,@,#,$,%,^,&,*,-,?,";
 var numberArray = "0,1,2,3,4,5,6,7,8,9,";
+// this part actually works:
+passWord();
+function passWord() {
+    passLength = prompt("Choose the length of your new password", "20");
+    console.log(passLength);
 
-if (lowerCheck.checked = true) {
-    charSet += lowerArray;
-    console.log(charSet);
+    
+    if ((passLength < 8) || (passLength > 128) || (isNaN(passLength))) {
+        alert("Please choose a password between 8 and 128 characters");
+        passWord();
+    }
 }
-if (upperCheck.checked = true) {
-    charSet += upperArray;
-    console.log(charSet);
-
-}
-if (numCheck.checked = true) {
-    charSet += numberArray;
-    console.log(charSet);
-
-}
-if (symbolCheck.checked = true) {
-    charSet += symbolArray;
-    console.log(charSet);
-
-}
-
-// document.genBtnEl.addEventListener("click", passGen);
-
-// function passGen() {
-
+// passConfirm();
+// function passConfirm(){
+// var includeUpper = confrim("Would you like uppercase letters?");
 // }
+
+// for (i = 1; i <= criteria.passwordLength; i++) {
+//     var index = Math.floor(Math.random() * options.length);
+//     newPassword = newPassword + options.charAt(index)
+//   }
+//   return newPassword;
